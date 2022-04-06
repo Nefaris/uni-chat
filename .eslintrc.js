444977840e9c0afd61e873e0f811fba117f8b638
Kamil Piękoś <kamil.piekos@10clouds.com>
2022-04-06 18:49:18 +0200

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+const App = () => (
+  <div>
+    <p>Hello world!</p>
+  </div>
+);
+
+export default App;
